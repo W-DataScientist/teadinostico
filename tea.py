7c3de8b65ae57e122b4a2246109bcfba2c923aeb
W-DataScientist <165705997+W-DataScientist@users.noreply.github.com>
2024-07-06 23:17:28 -0300

    Update tea.py

--- tea.py
+++ tea.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
-st.title("Questionário de Diagnóstico de Transtorno do Espectro Autista (TEA)")
+st.title("Questionário para pré diagnostico do (TEA)")
 
-st.write("Olá! Vamos começar com um questionário de 30 perguntas relacionadas ao TEA. Ao final, você poderá clicar no botão 'Diagnosticar' para obter o resultado.")
-
-st.subheader("Informações Pessoais")
+st.write("São 30 perguntas relacionadas ao TEA. Ao final, você poderá clicar no botão 'Diagnosticar' para obter o pré-diagnostico.")
 
 sex = st.radio("Sexo:", ("Masculino", "Feminino"), key="sex")
 age = st.slider("Idade:", min_value=1, max_value=60, step=1, key="age")
